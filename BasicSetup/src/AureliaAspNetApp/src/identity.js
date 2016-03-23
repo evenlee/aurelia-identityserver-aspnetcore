caee952842933e47cfcb1b486a931f6b608bc034
paul van bladel <paul.vanbladel@gmail.com>
2016-03-23 19:05:30 +0100

    fix identity

--- BasicSetup/src/AureliaAspNetApp/src/identity.js
+++ BasicSetup/src/AureliaAspNetApp/src/identity.js
@@ -1,6 +1,7 @@
 ﻿import {inject} from 'aurelia-framework';
 import {HttpClient} from 'aurelia-fetch-client';
 import 'isomorphic-fetch';
+import config from './authConfig';
 @inject(HttpClient)
 export class Identity{
     heading = 'Identity as known in the web api';
